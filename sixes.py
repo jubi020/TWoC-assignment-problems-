90e9dcacaa236be316d38565d2ff670b45c10fc2
jubi020 <jubikumari020@gmail.com>
2020-05-13 16:25:19 +0530

    assignment problem 5

---
+++ sixes.py
@@ -0,0 +1,18 @@
+a = int(input("enter the runs scored by player 1 in 60 balls"))
+b = int(input("enter the runs scored by player 2 in 60 balls"))
+c = int(input("enter the runs scored by player 3 in 60 balls"))
+s1 = (a / 60) * 100
+s2 = (b / 60) * 100
+s3 = (c / 60) * 100
+print("strike rate of 1st player is", s1)
+print("strike rate of 2nd player is", s2)
+print("strike rate of 3rd player is", s3)
+print(" runs made by player 1 in  60 more balls are",a)
+print(" runs made by player 2 in  60 more balls are",b)
+print(" runs made by player 13 in  60 more balls are",c)
+print("maximum no. of sixes shot by player 1 are",int(a/6)) 
+print("maximum no. of sixes shot by player 2 are",int(b/6))
+print("maximum no. of sixes shot by player 3 are",int(c/6))
+
+
+
