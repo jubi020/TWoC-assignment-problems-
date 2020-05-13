7b56d0862b4923c46f44b00d62bb2c0a4f22f743
jubi020 <jubikumari020@gmail.com>
2020-05-13 16:23:56 +0530

    assignment problem 4

---
+++ swapping.py
@@ -0,0 +1,8 @@
+a = int(input("enter first number to be swapped"))
+b = int(input("enter second number to be swapped"))
+a = a + b
+b = a - b
+a = a - b
+print("two numbers after swapping are :")
+print("a:", a)
+print("b:", b) 
